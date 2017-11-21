95710128c2f5bd074b4fd1e952c9be71a69409b2
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-11-21 11:21:46 +0100

    Added ability to have multiple responses to a single event

--- game/events.lua
+++ game/events.lua
@@ -3,31 +3,68 @@ local M = {}
 
 local events = {
 	{
-		image = "portrait1",
-		messenger = "Col. Dwayne Ronson",
-		message = "Sir, Pyongyang just launched a long-distance nucular missile test. What is your response?",
+		image = { "portrait1" },
+		messenger = { "Col. Dwayne Ronson", "Col. Sanders" },
+		message = {
+			"Sir, Pyongyang just launched a long-distance nucular missile test. What is your response?",
+			"Mr. President, Pyongyang just launched another long-distance nucular missile test. How should we respond?",
+		},
 		tweets = {
 			{
-				message = "One more from Rocket man, and we will bring down #FIRE and #FURY on Sou... North Korea!! #MURICA #eagle",
+				message = {
+					"One more from Rocket man, and we will bring down #FIRE and #FURY on Sou... North Korea!! #MURICA #eagle",
+					"Being nice to Rocket Man hasn't worked in 25 years, why would it work now? Clinton failed, Bush failed, and Obama failed. I won't fail.",
+				},
 				values = { -1, 1, -1, -1 },
 			},
 			{
-				message = "SAD! We want a PEACEFUL solution, so I'm sending my TOP diplomats (you know who you are) to fix the issue. #weshallovercome",
+				message = {
+					"SAD! We want a PEACEFUL solution, so I'm sending my TOP diplomats (you know who you are) to fix the issue. #weshallovercome",
+					"China is sending an Envoy and Delegation to North Korea - A big move, we'll see what happens!",
+				},
 				values = { 1, -1, 0, 0 },
 			}
 		},
 	},
 	{
-		image = "portrait1",
-		messenger = "Press secretary",
-		message = "The GOP has proposed a bill to stop gay marriage, what's your stance on the question?",
+		image = { "portrait1" },
+		messenger = { "Press secretary" },
+		message = {
+			"The GOP has proposed a bill to stop gay marriage, what's your stance on the question?",
+		},
 		tweets = {
 			{
-				message = "The Earth was made for Adam and Eve, NOT Adam and #Steve!",
+				message = {
+					"The Earth was made for Adam and Eve, NOT Adam and #Steve!",
+					"Maybe Shulman is having an affair with someone in the White House. He looks a little gay.",
+				},
 				values = { 0, 1, 0, 0 },
 			},
 			{
-				message = "I have several gay friends, SOME of who are VERY good people. They should get married!",
+				message = {
+					"I have several gay friends, SOME of who are VERY good people. They should get married!",
+				},
+				values = { 0, -1, 1, 0 },
+			}
+		},
+	},
+	{
+		image = { "portrait1" },
+		messenger = { "Senator Ted Cruz" },
+		message = {
+			"Trump & Rubio are w/Obama on gay marriage",
+		},
+		tweets = {
+			{
+				message = {
+					"Cruz is the worst liar, crazy or very dishonest. Perhaps all 3?",
+				},
+				values = { 0, 1, 0, 0 },
+			},
+			{
+				message = {
+					"If Ted Cruz is so opposed to gay marriage, why did he accept money from people who espouse gay marriage?",
+				},
 				values = { 0, -1, 1, 0 },
 			}
 		},
