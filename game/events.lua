e2f10e11ee6b7a3907d9f2fcb7aaa94b613135ea
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-11-21 08:48:41 +0100

    Game loop

--- game/events.lua
+++ game/events.lua
@@ -4,14 +4,30 @@ local M = {}
 local events = {
 	{
 		image = "portrait1",
-		message = "North Korea missile launch",
+		messenger = "Col. Dwayne Ronson",
+		message = "Sir, Pyongyang just launched a long-distance nucular missile test. What is your response?",
 		tweets = {
 			{
-				message = "Being nice to Rocket Man hasn't worked in 25 years, why would it work now? Clinton failed, Bush failed, and Obama failed. I won't fail.",
-				values = { -1, 1, 0, -1 },
+				message = "One more from Rocket man, and we will bring down #FIRE and #FURY on Sou... North Korea!! #MURICA #eagle",
+				values = { -1, 1, -1, -1 },
 			},
 			{
-				message = "Let's try the diplomatic way first.",
+				message = "SAD! We want a PEACEFUL solution, so I'm sending my TOP diplomats (you know who you are) to fix the issue. #weshallovercome",
+				values = { 1, -1, 0, 0 },
+			}
+		},
+	},
+	{
+		image = "portrait1",
+		messenger = "Press secretary",
+		message = "The GOP has proposed a bill to stop gay marriage, what's your stance on the question?",
+		tweets = {
+			{
+				message = "The Earth was made for Adam and Eve, NOT Adam and #Steve!",
+				values = { 0, 1, 0, 0 },
+			},
+			{
+				message = "I have several gay friends, SOME of who are VERY good people. They should get married!",
 				values = { 0, -1, 1, 0 },
 			}
 		},
@@ -23,7 +39,12 @@ local events = {
 function M.new_game()
 	local ev = {}
 	for i=1,#events do
-		ev[math.random(1,#events)] = events[i]
+		ev[i] = events[i]
+	end
+	for i=1,#ev do
+		local a = math.random(1, #ev)
+		local b = math.random(1, #ev)
+		ev[a], ev[b] = ev[b], ev[a]
 	end
 	return ev
 end
